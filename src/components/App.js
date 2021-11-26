0db29faa448f3b605fe2b4ae6ee39f538e0b1de9
Krystie-W <tsukio.scoring@gmail.com>
2021-11-26 11:03:37 +1100

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Header from "./Header";
+import Article from "./Article";
+
 
 function App() {
-  return <div className="App">learn react!</div>;
+  return (
+  <div>
+  <Header />
+  <div className="App">learn react!</div>
+  <Article />
+  </div>
+  );
 }
+
 
 export default App;
